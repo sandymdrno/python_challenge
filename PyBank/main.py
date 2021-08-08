bbe58894c9ff1c382912049cd49a8be909107bab
Sandra Medrano <87509694+sandymdrno@users.noreply.github.com>
2021-08-07 23:32:19 -0500

    Python Challenge Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,7 +13,15 @@ csvpath = Path('./Resources/budget_data.csv')
 # Lists to store data
 total_NR = 0
 date_list =[]
-amount_list =[]
+profit =[]
+#increase_date = []
+#decrease_date = []
+date = []
+initial_profit = 0
+final_profit = 0 
+total_change_profits = 0
+monthly_changes = []
+
 with open(csvpath, "r") as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -30,14 +38,28 @@ with open(csvpath, "r") as csvfile:
     # Read each row of data after the header
 
     for row in csvreader: 
-        if count >=0:
-            #print (row)
-            total_NR += int(row[1])
-            amount_list = row[1]
-            print(amount_list)
-        count +=1 
+        
+        total_NR += int(row[1])
+        profit = row[1]
+        final_profit = int(row[1])
+        monthly_change_profits = final_profit - initial_profit
+        monthly_changes.append(monthly_change_profits)
+        total_change_profits = total_change_profits + monthly_change_profits
+        initial_profit = final_profit
+        count +=1
+        average_change_profits = (total_change_profits/count)
+        greatest_increase_profits = max(monthly_changes)
+        greatest_decrease_profits = min(monthly_changes)
+        #increase_date = date[monthly_changes.index(greatest_increase_profits)]
+        #decrease_date = date[monthly_changes.index(greatest_decrease_profits)]
 
+            
+       
+print("Financial Analysis")
+print("------------------------------------")
 print("Total Months: " + str(count))
 print("Total: $" + str(total_NR))
-print("Average Change: $" + str(total_NR/count))
-print("Greatest Increase in Profits:" + str(max(amount_list)))
+print("Average Change: " + "$" + str(int(average_change_profits)))
+print("Greatest Increase in Profits:  $" + str(greatest_increase_profits) )
+print("Greatest Decrease in Profits: $"  + str(greatest_decrease_profits) )
+print("------------------------------------")
